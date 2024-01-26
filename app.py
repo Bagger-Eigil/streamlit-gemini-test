4a8536923d6c85edfe03cf1679de40d7dccfc81d
Eigil Bagger <bagger.eigil@bcg.com>
2024-01-26 14:54:05 +0100

    Feature: Remove Google API dependency

--- app.py
+++ app.py
@@ -4,31 +4,32 @@ import streamlit as st
 import google.generativeai as genai
 import google.ai.generativelanguage as glm
 
-with st.sidebar:
-    st.title("Gemini API")
+# with st.sidebar:
+#     st.title("Gemini API")
     
-    api_key = st.text_input("API key")
-    if api_key:
-        genai.configure(api_key=api_key)
-    else:
-        if "api_key" in st.secrets:
-            genai.configure(api_key=st.secrets["api_key"])
-        else:
-            st.error("Missing API key.")
+#     api_key = st.text_input("API key")
+#     if api_key:
+#         genai.configure(api_key=api_key)
+#     else:
+#         if "api_key" in st.secrets:
+#             genai.configure(api_key=st.secrets["api_key"])
+#         else:
+#             st.error("Missing API key.")
     
-    select_model = st.selectbox("Select model", ["gemini-pro", "gemini-pro-vision"])
-    if select_model == "gemini-pro-vision":
-        uploaded_image = st.file_uploader(
-            "upload image",
-            label_visibility="collapsed",
-            accept_multiple_files=False,
-            type=["png", "jpg"],
-        )
-        st.caption(
-            "Note: The vision model gemini-pro-vision is not optimized for multi-turn chat."
-        )
-        if uploaded_image:
-            image_bytes = uploaded_image.read()
+#     select_model = st.selectbox("Select model", ["gemini-pro", "gemini-pro-vision"])
+#     if select_model == "gemini-pro-vision":
+#         uploaded_image = st.file_uploader(
+#             "upload image",
+#             label_visibility="collapsed",
+#             accept_multiple_files=False,
+#             type=["png", "jpg"],
+#         )
+#         st.caption(
+#             "Note: The vision model gemini-pro-vision is not optimized for multi-turn chat."
+#         )
+#         if uploaded_image:
+#             image_bytes = uploaded_image.read()
+
 
 def get_response(messages, model="gemini-pro"):
     model = genai.GenerativeModel(model)
@@ -50,6 +51,8 @@ if messages and select_model != "gemini-pro-vision":
             st.chat_message("assistant").markdown(parts[0])
 
 chat_message = st.chat_input("Say something")
+
+st.write("Here's a message for you")
 
 if chat_message:
     st.chat_message("user").markdown(chat_message)
